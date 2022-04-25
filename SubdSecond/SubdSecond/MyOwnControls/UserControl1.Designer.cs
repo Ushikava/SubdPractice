8427e303a03c29bb93890749a704bc942a9b87ed
Ushikava <bryuhanov-aleksey@yandex.ru>
2022-04-25 13:45:38 +0300

    NewUpdate
    =_=

--- SubdSecond/SubdSecond/MyOwnControls/UserControl1.Designer.cs
+++ SubdSecond/SubdSecond/MyOwnControls/UserControl1.Designer.cs
@@ -28,18 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.bookStatusLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bookNameLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // bookStatusLabel
+            // 
+            this.bookStatusLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.bookStatusLabel.Location = new System.Drawing.Point(0, 0);
+            this.bookStatusLabel.Name = "bookStatusLabel";
+            this.bookStatusLabel.Size = new System.Drawing.Size(230, 25);
+            this.bookStatusLabel.TabIndex = 0;
+            this.bookStatusLabel.Text = "label1";
+            this.bookStatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::SubdSecond.Properties.Resources.dog;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(230, 230);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // bookNameLabel
+            // 
+            this.bookNameLabel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.bookNameLabel.Location = new System.Drawing.Point(0, 255);
+            this.bookNameLabel.Name = "bookNameLabel";
+            this.bookNameLabel.Size = new System.Drawing.Size(230, 25);
+            this.bookNameLabel.TabIndex = 2;
+            this.bookNameLabel.Text = "label1";
+            this.bookNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // singleBookPanelControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.bookNameLabel);
+            this.Controls.Add(this.bookStatusLabel);
             this.Name = "singleBookPanelControl";
             this.Size = new System.Drawing.Size(230, 280);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label bookStatusLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label bookNameLabel;
     }
 }
--- SubdSecond/SubdSecond/Properties/Resources.Designer.cs
+++ SubdSecond/SubdSecond/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace SubdSecond.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap dog {
+            get {
+                object obj = ResourceManager.GetObject("dog", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
